83599ad35320bfa9f9bb334ac80fa96f453ffa43
Mohd Shahid <155215858+shahid-mohd@users.noreply.github.com>
2025-12-12 19:44:19 +0530

    Add SQL query to calculate immediate delivery percentage

---
+++ Session_4/query.sql
@@ -0,0 +1,10 @@
+-- G3_23BCS10258_Mohd_Shahid
+
+SELECT ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) 
+AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+  SELECT customer_id, min(order_date) 
+  FROM Delivery
+  GROUP BY customer_id
+);
